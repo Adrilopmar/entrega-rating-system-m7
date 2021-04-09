a62fed84666510034b4da8a0a7cdb85b3772c4cd
Adrià <adria.lop.mar@gmail.com>
2021-04-09 11:16:44 +0200

    codi optimitzat (update)

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
+document.getElementById('contingut').addEventListener('click', bor);
+var prova2 = document.getElementsByTagName('input')
+function bor(e){
+    if(e.target.id == 'contingut' || e.target.id =='systemR'){
+        for(i=0;i<prova2.length;i++){
+            prova2[i].checked=false;
+        }
+        
+    }
+}
+
+/*
 document.getElementById('systemR').addEventListener('mouseover', quantes);
 document.getElementById('systemR').addEventListener('mouseout',quines );
 document.getElementById('systemR').addEventListener('click', pinta);
@@ -6,13 +18,13 @@ var galaxy =[document.getElementById('star-1'),
             document.getElementById('star-2'),
             document.getElementById('star-3'),
             document.getElementById('star-4'),
-            document.getElementById('star-5')]
+            document.getElementById('star-5')];
 var stars = [document.getElementById('star-11'),
             document.getElementById('star-12'),
             document.getElementById('star-13'),
             document.getElementById('star-14'),
-            document.getElementById('star-15')]
-function quantes(e){
+            document.getElementById('star-15')];
+/*function quantes(e){
     if(e.target.firstChild.value >0){
         for(i=0;i<galaxy.length;i++){
             if(e.target.firstChild.value>= galaxy[i].value){
@@ -47,5 +59,15 @@ function begin(e){
         galaxy[i].checked = false;
         stars[i].classList.remove('rated')
         stars[i].classList.remove('painted')
+        alert('asasdadasdas')
     }
 }
+
+function begin(e){
+    if(e.target.id == 'contingut' || e.target.id == 'systemR'){
+        var prova= document.getElementsByTagName('input');
+        prova.checked= false;
+        alert('asd');
+        //galaxy[i].checked = false;  
+    }
+}*/
